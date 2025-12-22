25fc170183ec8b9d56df977eee1a3880bc33e1c8
wangfenghuan <wfenghuan@foxmail.com>
2025-12-22 19:18:11 +0800

    diagram upload to minio

--- src/main/java/com/wfh/drawio/model/enums/FileUploadBizEnum.java
+++ src/main/java/com/wfh/drawio/model/enums/FileUploadBizEnum.java
@@ -13,7 +13,8 @@ import org.apache.commons.lang3.ObjectUtils;
  */
 public enum FileUploadBizEnum {
 
-    USER_AVATAR("用户头像", "user_avatar");
+    USER_AVATAR("用户头像", "user_avatar"),
+    DIAGRAM("图表", "diagram");
 
     private final String text;
 
