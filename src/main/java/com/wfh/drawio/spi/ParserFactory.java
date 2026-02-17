194a34cd64ae1f0397229ab76e0cb1ecf3208bdc
wangfenghuan <wfenghuan@foxmail.com>
2026-02-17 20:16:11 +0800

    feat: Introduce a pluggable language parsing SPI with Java Spring and SQL implementations, architectural models, and a new code parsing controller, refactoring `SpringBootJavaParserService` to utilize the SPI

---
+++ src/main/java/com/wfh/drawio/spi/ParserFactory.java
@@ -0,0 +1,11 @@
+package com.wfh.drawio.spi;
+
+/**
+ * @Title: ParserFactory
+ * @Author  wangfenghuan
+ * @Package com.wfh.drawio.spi
+ * @Date  2026/2/17 19:22
+ * @description: 
+*/
+public class ParserFactory {
+}
---
+++ src/main/java/com/wfh/drawio/spi/parser/JavaSpringParser.java
@@ -0,0 +1,11 @@
+package com.wfh.drawio.spi.parser;
+
+/**
+ * @Title: JavaSpringParser
+ * @Author  wangfenghuan
+ * @Package com.wfh.drawio.spi.parser
+ * @Date  2026/2/17 19:25
+ * @description: 
+*/
+public class JavaSpringParser {
+}
